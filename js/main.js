b9517fb6da1a032144ad035be9b70329e9240dde
MoorePower <mikmoor@gmail.com>
2013-11-30 06:09:18 -0700

    Project Container test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
+	//button model
 	$('.ui-btn').each(function(){
 		
 		var rel = $(this).attr('rel');
-		
+		//set the button icon based on the rel tag
 		$(this).button({
 			icons: {
 				primary: rel
@@ -12,12 +13,15 @@ $(document).ready(function(){
 	});
 	
 	$('#btnAdd').click(function(){
-		
+		$("<p>test</p>").appendTo(".main");
 	});
 	
 	$('#btnRemove').click(function(){
 		
 	});
+	
+	$('.main').sortable();
+	$('.main').disableSelection();
 
 });
 
